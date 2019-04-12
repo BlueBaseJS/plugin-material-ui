e6c98cbe4573f96aa53a7449f78e56f21804dc4f
adnan1naeem <adnan1naeem@gmail.com>
2019-04-12 16:49:02 +0500

    feat(added avatar components stories): stories of avatar

--- src/components/Avatar/Avatar.tsx
+++ src/components/Avatar/Avatar.tsx
@@ -1,10 +1,10 @@
-// import { AvatarDefaultProps } from '@bluebase/components';
 import Avatar from '@material-ui/core/Avatar';
+import { AvatarDefaultProps } from '@bluebase/components';
 import { componentMapper } from '@bluebase/component-mapper';
 
 const AvatarComponent = componentMapper(Avatar, {
 	sizes: 'size',
 	src: 'source'
-}, { rest: true, });
+}, { rest: true, defaultProps: AvatarDefaultProps });
 
 export { AvatarComponent as Avatar };
