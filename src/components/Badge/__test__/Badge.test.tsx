433906576c1e6dfc8c7d852a03b521b57d0c3a53
Muhammad Husnain Naseer <mhusnainnaseer786@gmail.com>
2019-04-23 14:45:36 +0500

    test(Test Cases): Test cases for BAdge component are added.

---
+++ src/components/Badge/__test__/Badge.test.tsx
@@ -0,0 +1,22 @@
+import { Badge } from '../Badge';
+import React from 'react';
+import { shallow } from 'enzyme';
+
+test('Badge component with visible Prop', () => {
+	const component = shallow(
+		<Badge visible = {false}/>
+	);
+	// expect(component).toMatchSnapshot();
+	expect(component.props().visible).toEqual(false);
+});
+
+// test('Badge component with Children', () => {
+// 	const component = shallow(
+// 		<Badge>
+//             <Text></Text>
+//         </Badge>
+//     );
+//     console.log(component.children());
+// 	// expect(component).toMatchSnapshot();
+// 	expect(component.children()).toEqual(5);
+// });
