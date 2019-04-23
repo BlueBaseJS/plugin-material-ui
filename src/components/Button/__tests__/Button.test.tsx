0c5cdaa3907dfabbef375d154345b135d681b531
adnan1naeem <adnan1naeem@gmail.com>
2019-04-23 10:52:15 +0500

    test(Button component): Button component  tests are fixed

--- src/components/Button/__tests__/Button.test.tsx
+++ src/components/Button/__tests__/Button.test.tsx
@@ -6,17 +6,53 @@ import { shallow } from 'enzyme';
 
 test('Button component should use title prop to show children', () => {
 	const component = shallow(
-    <Button title="Foo" />
+		<Button title="title" />
 	);
-	// expect(component).toMatchSnapshot();
-	expect(component.childAt(0).text()).toEqual('Foo');
+	expect(component.props().title).toEqual('title');
 });
+
+
 
 test('Button component should use children prop to show content', () => {
 	const component = shallow(
-    <Button title="Foo">
-		<Text>Bar</Text>
-	</Button>
-  );
-	expect(component.childAt(0).childAt(0).text()).toEqual('Bar');
+		<Button title="Foo" loading={true} />
+	);
+	expect(component.props().loading).toEqual(true);
+});
+
+
+
+test('Button component should use title prop to show content', () => {
+	const component = shallow(
+		<Button title="Foo" loading={false} />
+	);
+	expect(component.props().title).toEqual('Foo');
+});
+
+
+
+test('Button component should  return loading true', () => {
+	const component = shallow(
+		<Button loading={true}>
+			<Text>Bar</Text>
+		</Button>
+	);
+	expect(component.props().children).toBeDefined();
+});
+
+
+test('Button component should return loading false', () => {
+	const component = shallow(
+		<Button loading={false}>
+			<Text>Bar</Text>
+		</Button>);
+	expect(component.props().loading).toEqual(false);
+});
+
+
+
+test('Button component should use children prop to show content', () => {
+	const wrapper = shallow(
+		<Button />);
+	expect(wrapper.type()).toBeFalsy();
 });
