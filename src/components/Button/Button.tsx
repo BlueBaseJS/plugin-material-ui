5186fceca155d4cbd7ed0d051cb7ccc91d925cff
Asad Razzaq <asad.rzaq@gmail.com>
2019-04-25 18:55:26 +0500

    fix(TS Lint): tslint issues fixed

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -3,72 +3,70 @@ import {
 	ButtonProps
 } from '@bluebase/components';
 import MUIButton from '@material-ui/core/Button';
-import { withPropsStyles } from '../../withPropsStyles';
-import React from "react"
-
-
+import React from 'react';
 import { objectMapper } from '@bluebase/component-mapper';
+import { withPropsStyles } from '../../withPropsStyles';
 
 
 
 
-const styles = ({ color  }: any, theme: any) => {
 
-	console.log("theme",theme);
+
+const styles = ({ color }: any, theme: any) => {
+
 	switch (color) {
 		case 'primary':
-		return{};
+			return {};
 		case 'secondary':
-		return{};
+			return {};
 		case 'success':
-		return({
-			root: {
-				// color,
-				color:theme.palette.success.contrastText,
-				backgroundColor:theme.palette.success.main,
-				// tslint:disable-next-line: object-literal-sort-keys
-				'&$hover': {
-					color:theme.palette.success.contrastText,
-				},
-				
-			}});
+			return ({
+				root: {
+					backgroundColor: theme.palette.success.main,
+					color: theme.palette.success.contrastText,
+					// tslint:disable-next-line: object-literal-sort-keys
+					'&$hover': {
+						color: theme.palette.success.contrastText,
+					},
+
+				}
+			});
 		case 'error':
-	
-		return({
-			root: {
-				// color,
-				color:theme.palette.success.contrastText,
-				backgroundColor:theme.palette.error.main,
-				// tslint:disable-next-line: object-literal-sort-keys
-				'&$hover': {
-					color:theme.palette.error.contrastText,
-				},
-				'&$disabled': {
-					color: theme.palette.action.disabled,
-				},
-			}});
+
+			return ({
+				root: {
+					backgroundColor: theme.palette.error.main,
+					color: theme.palette.success.contrastText,
+					// tslint:disable-next-line: object-literal-sort-keys
+					'&$hover': {
+						color: theme.palette.error.contrastText,
+					},
+					'&$disabled': {
+						color: theme.palette.action.disabled,
+					},
+				}
+			});
 		case 'warning':
-		return({
-			root: {
-				color:theme.palette.success.contrastText,
-				backgroundColor:theme.palette.warning.main,
-				// tslint:disable-next-line: object-literal-sort-keys
-				'&$hover': {
-					color:theme.palette.warning.contrastText,
-					
-				},
-				'&$disabled': {
-					color: theme.palette.action.disabled,
-				},
-			}});			
-			//break;
-	
+			return ({
+				root: {
+					backgroundColor: theme.palette.warning.main,
+					color: theme.palette.success.contrastText,
+					// tslint:disable-next-line: object-literal-sort-keys
+					'&$hover': {
+						color: theme.palette.warning.contrastText,
+
+					},
+					'&$disabled': {
+						color: theme.palette.action.disabled,
+					},
+				}
+			});
 		default:
 			return null;
 	}
 
-	
-};  
+
+};
 const map = {
 	// If color is primary, secondary or default set as is
 	color: ({ color }: ButtonProps) => {
@@ -95,7 +93,7 @@ export const Button = withPropsStyles(styles)((props: any) => {
 
 	const newProps = objectMapper(props, map, { rest: true, ignore: ['onValueChange'] });
 
-	const { loading, color,children, title, classes, indeterminate, ...common } = newProps;
+	const { loading, color, children, title, classes, indeterminate, ...common } = newProps;
 
 	if (Object.keys(classes).length > 0) {
 		delete common.color;
@@ -105,7 +103,7 @@ export const Button = withPropsStyles(styles)((props: any) => {
 	return (
 		<MUIButton style={classes} {...props}>
 
-		{props.children?props.children:props.title}
-	</MUIButton>
+			{props.children ? props.children : props.title}
+		</MUIButton>
 	);
 }) as any;
