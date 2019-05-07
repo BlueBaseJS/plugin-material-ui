9eeed63a926403e3f1d3881eb9c0cc2f771242de
Abdul Rehman Talat <rehman.talat@gmail.com>
2019-05-07 21:59:30 +0500

    fix: Removed console warnings

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -38,6 +38,6 @@ export const Button = withPropsStyles(styles)(componentMapper<ButtonProps>(MUIBu
 		);
 	},
 	onClick: 'onPress',
-}, { rest: true, })) as React.ComponentType<ButtonProps>;
+}, { rest: true, ignore: ['color', 'loading'] })) as React.ComponentType<ButtonProps>;
 
 Button.defaultProps = ButtonDefaultProps;
