95c19946b4115d5c4c33c0833dbdcf3ee814345d
Abdul Rehman Talat <rehman.talat@gmail.com>
2019-07-16 23:22:19 +0500

    fix(BottomNavigationAction): Removed extra props

--- src/components/BottomNavigationAction/BottomNavigationAction.tsx
+++ src/components/BottomNavigationAction/BottomNavigationAction.tsx
@@ -1,29 +1,36 @@
 import { BottomNavigationActionProps, DynamicIcon } from '@bluebase/components';
+
 import MuiBottomNavigationAction from '@material-ui/core/BottomNavigationAction';
 import React from 'react';
+import { StyleSheet } from 'react-native';
 import { componentMapper } from '@bluebase/component-mapper';
 
-export const BottomNavigationAction = componentMapper<BottomNavigationActionProps>(MuiBottomNavigationAction, {
-	icon: ({ icon }) => {
+export const BottomNavigationAction = componentMapper<BottomNavigationActionProps>(
+	MuiBottomNavigationAction,
+	{
+		icon: ({ icon }) => {
+			if (!icon) {
+				return;
+			}
 
-		if (!icon) {
-			return;
-		}
+			const size = icon.size || 24;
 
-		const size = icon.size || 24;
+			const props = {
+				size,
+				style: {
+					// lineHeight: size,
+					...icon.style,
+				},
+				...icon,
+			};
 
-		const props = {
-			size,
-			style: {
-				lineHeight: size,
-				...icon.style,
-			},
-			...icon,
-		};
+			return React.createElement(DynamicIcon, props);
+		},
 
-		return React.createElement(DynamicIcon, props);
+		style: ({ style }: any) => StyleSheet.flatten(style),
+
+		label: 'label',
+		showLabel: 'showLabel',
+		value: 'value',
 	}
-}, {
-	rest: true
-});
-
+);
