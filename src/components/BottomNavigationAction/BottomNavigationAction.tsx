c276b2c2714df6ec3301438d86878287548b07ee
adnan.naeem@blueeast.com <adnan.naeem@blueeast.com>
2019-08-19 15:56:03 +0500

    fix(picker ): tslint
    tslint issues resolved

--- src/components/BottomNavigationAction/BottomNavigationAction.tsx
+++ src/components/BottomNavigationAction/BottomNavigationAction.tsx
@@ -27,11 +27,11 @@ export const BottomNavigationAction = componentMapper<BottomNavigationActionProp
 			return React.createElement(DynamicIcon, props);
 		},
 
+		onChange: 'onChange',
 		style: ({ style }: any) => StyleSheet.flatten(style),
 
 		label: 'label',
 		showLabel: 'showLabel',
 		value: 'value',
-		onChange: 'onChange'
 	}
 );
