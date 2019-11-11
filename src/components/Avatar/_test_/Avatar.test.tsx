41baeeafd34d6dd274dbab03ba034abfd0505995
Abdul Rehman Talat <rehman.talat@gmail.com>
2019-11-12 00:08:25 +0500

    test(Avatar): Fixed coverage

--- src/components/Avatar/_test_/Avatar.test.tsx
+++ src/components/Avatar/_test_/Avatar.test.tsx
@@ -7,87 +7,66 @@ import { waitForElement } from 'enzyme-async-helpers';
 
 const path = 'https://via.placeholder.com/300/09f.png%20C/O%20https://placeholder.com/';
 
-
-
-
 test('avatar component should  return  source Image', async () => {
-
-
 	const wrapper = mount(
-    <BlueBaseApp plugins={[Plugin]}>
-
-      <Avatar
-        type="image"
-        image={{ uri: path }}
-      />
-
-    </BlueBaseApp>
-  );
+		<BlueBaseApp plugins={[Plugin]}>
+			<Avatar type="image" image={{ uri: path }} />
+		</BlueBaseApp>
+	);
 
 	await waitForElement(wrapper, Avatar);
 
-	const sourceProp: any = wrapper.find('Avatar BlueBaseImage').first().prop('source');
+	const sourceProp: any = wrapper
+		.find('Avatar BlueBaseImage')
+		.first()
+		.prop('source');
 	expect(sourceProp.uri).toBe(path);
-
 });
 
-
-
 test('avatar component should  return  source Image', async () => {
-
-
 	const wrapper = mount(
-    <BlueBaseApp plugins={[Plugin]}>
-
-      <Avatar
-        type="image"
-        image={path as any}
-      />
-
-    </BlueBaseApp>
-  );
+		<BlueBaseApp plugins={[Plugin]}>
+			<Avatar type="image" image={path as any} />
+		</BlueBaseApp>
+	);
 
 	await waitForElement(wrapper, Avatar);
 
-	const sourceProp: any = wrapper.find('Avatar BlueBaseImage').first().prop('source');
+	const sourceProp: any = wrapper
+		.find('Avatar BlueBaseImage')
+		.first()
+		.prop('source');
 	expect(sourceProp).toBe(path);
-
 });
-
 
 test('avatar component should  return   type =icon', async () => {
-
 	const wrapper = mount(
-    <BlueBaseApp plugins={[Plugin]}>
-      <Avatar type="icon" icon="inbox" />
-    </BlueBaseApp>
-  );
+		<BlueBaseApp plugins={[Plugin]}>
+			<Avatar type="icon" icon="inbox" />
+		</BlueBaseApp>
+	);
 
 	await waitForElement(wrapper, Avatar);
 
-	const sourceProp: any = wrapper.find('Avatar Icon').last().prop('name');
+	const sourceProp: any = wrapper
+		.find('Avatar Icon')
+		.last()
+		.prop('name');
 	expect(sourceProp).toBe('inbox');
-
 });
-
-
 
 test('avatar component should  return  source Image', async () => {
-
-
 	const wrapper = mount(
-    <BlueBaseApp plugins={[Plugin]}>
-      <Avatar type="text" text="N" />
-
-
-    </BlueBaseApp>
-  );
+		<BlueBaseApp plugins={[Plugin]}>
+			<Avatar type="text" text="N" color="primary" />
+		</BlueBaseApp>
+	);
 
 	await waitForElement(wrapper, Avatar);
 
-	const sourceProp: any = wrapper.find('Avatar').first().text();
+	const sourceProp: any = wrapper
+		.find('Avatar')
+		.first()
+		.text();
 	expect(sourceProp).toBe('N');
-
 });
-
-
