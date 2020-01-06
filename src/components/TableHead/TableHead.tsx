09c87d2c00ae1da8e5bd2e767f862ef0db198d81
Abdul Rehman Talat <rehman.talat@gmail.com>
2020-01-06 12:03:54 +0500

    fix(Table): Fixed table bugs

--- src/components/TableHead/TableHead.tsx
+++ src/components/TableHead/TableHead.tsx
@@ -1,11 +1,14 @@
 import { TableHeadDefaultProps, TableHeadProps } from '@bluebase/components';
+
 import MuiTableHead from '@material-ui/core/TableHead';
+import MuiTableRow from '@material-ui/core/TableRow';
 import React from 'react';
+
 export const TableHead = (props: TableHeadProps) => {
 	const { style, children, ...rest } = props;
 	return (
 		<MuiTableHead style={style as any} {...rest}>
-			{children}
+			<MuiTableRow>{children}</MuiTableRow>
 		</MuiTableHead>
 	);
 };
--- src/components/TableTitle/TableTitle.tsx
+++ src/components/TableTitle/TableTitle.tsx
@@ -1,12 +1,15 @@
 import { TableTitleDefaultProps, TableTitleProps } from '@bluebase/components';
+
+import MuiTableCell from '@material-ui/core/TableCell';
 import MuiTableSortLabel from '@material-ui/core/TableSortLabel';
 import React from 'react';
+
 export const TableTitle = (props: TableTitleProps) => {
 	const { direction, style, children, ...rest } = props;
 	return (
-		<MuiTableSortLabel direction={direction as any} style={style as any} {...rest}>
-			{children}
-		</MuiTableSortLabel>
+		<MuiTableCell style={style as any} {...rest}>
+			<MuiTableSortLabel direction={direction as any}>{children}</MuiTableSortLabel>
+		</MuiTableCell>
 	);
 };
 TableTitle.defaultProps = TableTitleDefaultProps;
