175da9c9ef90a98192b0eb9781846a9656b90d7c
adnan1naeem <adnan1naeem@gmail.com>
2020-07-23 14:44:26 +0500

    fix(resolved issue of Avatar component): resolved issue of Avatar component

--- src/components/ListAvatar/ListAvatar.tsx
+++ src/components/ListAvatar/ListAvatar.tsx
@@ -1,3 +1,11 @@
+import  * as React from  'react'
 import { Avatar } from '@bluebase/components';
+import ListItemAvatar from '@material-ui/core/ListItemAvatar';
+export const ListAvatar = (props)=>{
+    return(
+        <ListItemAvatar>
+            <Avatar  {...props} />
+        </ListItemAvatar>
 
-export const ListAvatar = Avatar;
+    );
+}
