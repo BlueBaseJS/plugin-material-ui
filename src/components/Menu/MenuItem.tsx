db0d8479b32bd4102f06093bafa1803dc89835dd
Abdul Rehman Talat <rehman.talat@gmail.com>
2022-07-24 02:58:53 +0500

    feat(MenuItem): Improved styling

--- src/components/Menu/MenuItem.tsx
+++ src/components/Menu/MenuItem.tsx
@@ -1,17 +1,27 @@
 import { DynamicIcon, MenuItemDefaultProps, MenuItemProps } from '@bluebase/components';
+import { useTheme } from '@bluebase/core';
 import MuiMenuItem from '@material-ui/core/MenuItem';
 import React from 'react';
 import { StyleSheet } from 'react-native';
 
 export const MenuItem = (props: MenuItemProps) => {
 	const { title, disabled, icon, onPress, style } = props;
+	const { theme } = useTheme();
+
 	return (
 		<MuiMenuItem
 			disabled={disabled}
 			onClick={onPress}
 			style={StyleSheet.flatten(style) as any}
 		>
-			{icon ? <DynamicIcon {...icon} /> : null}
+			{icon ? (
+				<DynamicIcon
+					size={20}
+					style={{ marginRight: theme.spacing.unit }}
+					color={theme.palette.text.secondary}
+					{...icon}
+				/>
+			) : null}
 			{title}
 		</MuiMenuItem>
 	);
